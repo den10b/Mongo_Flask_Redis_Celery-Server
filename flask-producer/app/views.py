36298b4ea82e6cdbae6b6e6e36012439bd5c7646
den10b <den10b@yandex.ru>
2022-06-07 00:21:45 +0300

    celery Flask Mongo REDIS

--- flask-producer/app/views.py
+++ flask-producer/app/views.py
@@ -3,22 +3,16 @@ import flask
 from . import app
 from . import cel_app
 
+
 @app.route('/post', methods=['POST'])
 def sendMessage():
     username = flask.request.values.get('user')
     message = flask.request.values.get('message')
     r = cel_app.send_task('tasks.Send_Message', kwargs={'user': username, 'mess': message})
-    # Message(name=username, message=message).save()
     return str(cel_app.AsyncResult(str(r.id)).result)
 
 
 @app.route('/get', methods=['GET'])
 def getChat():
     r = cel_app.send_task('tasks.Get_Message')
-
     return str(cel_app.AsyncResult(str(r.id)).result)
-    # msg = ""
-    # for obj in Message.objects():
-    #     msg += str(obj.name + ": " + obj.message)
-    #     msg += '<br>'
-    # return msg
--- mongo-consumer/worker.py
+++ mongo-consumer/worker.py
@@ -1,7 +1,8 @@
 from mongoengine import *
 from celery import Celery
+from redis import Redis
 
-connect('messages', host='mongodb', port=27017)
+redis = Redis(host='redis', port=6379, db=0)
 
 
 class Message(Document):
@@ -12,19 +13,41 @@ class Message(Document):
 app = Celery('tasks',
              broker='amqp://user:pass@rabbit:5672',
              backend='mongodb://mongodb:27017/backdb')
+
+
 @app.task()
 def Send_Message(user, mess):
-    Message(name=user, message=mess).save()
-    print(user,mess)
+    try:
+        connect('messages', host='mongodb', port=27017)
+        Message(name=user, message=mess).save()
+    except:
+        pass
+    print(user, mess)
+    if redis.get("cache") is not None:
+        redis.delete("cache")
+
     return "Send message"
 
 
 @app.task()
 def Get_Message():
     res = ""
-    for obj in Message.objects():
-        res += str(obj.name + ": " + obj.message + " <br>")
+
+    if redis.get("cache") is not None:
+        res = (str(redis.get("cache")))[2:]
+        print("From Redis")
+    else:
+        try:
+            connect('messages', host='mongodb', port=27017)
+            res = ""
+            for obj in Message.objects():
+                res += str(obj.name + ": " + obj.message + " <br>")
+        except:
+            pass
+        redis.set("cache", res)
+        print("From BD")
     return res
+
 
 if __name__ == '__main__':
     app.start(["worker"])
